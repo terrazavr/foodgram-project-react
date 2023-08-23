9ca99053db7b53973470478055b3244e14ef9f21
Di <120574516+terrazavr@users.noreply.github.com>
2023-08-23 23:26:30 +0900

    workflowfile

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="SECRET_KEY")
 
 DEBUG = config("DEBUG", cast=bool)
 
